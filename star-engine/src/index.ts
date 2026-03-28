95ce47adbe222b4c98a967ed024395513f12c82a
uo300652 <uo300652@uniovi.es>
2026-03-28 16:18:18 +0100

    Fix export problems

--- star-engine/src/index.ts
+++ star-engine/src/index.ts
@@ -1,3 +1,3 @@
-export * from './coordinates';
-export * from './stars';
-export * from './constellations';
+export * from './coordinates.js';
+export * from './stars.js';
+export * from './constellations.js';
